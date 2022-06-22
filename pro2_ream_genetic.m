0cc5b29b985549fa3171d965bf7ba42086c35fe7
LRBLRBLRB <550625844@qq.com>
2022-06-23 01:56:31 +0800

    20220623 ga

--- pro2_ream_genetic.m
+++ pro2_ream_genetic.m
@@ -12,28 +12,34 @@ m2 = 0.5;
 J1 = m1*L1^2/12;
 J2 = m2*L2^2/12;
 
-tau1 = 0.05;
-tau2 = 0.05;
-
 %% matlab genetic algorithm function
 l1 = 0.05; % begin with the original parameters
 l2 = 0.025;
-
-
-x  = ga(@fun,2);
-
-
-%% 
-function te = fun(l)
 tau1 = 0.05;
 tau2 = 0.05;
+
+lb = [0;0];
+ub = [L1;L2];
+optimOpts = optimoptions('ga', ...
+    'UseParallel',true, ...
+    'PlotFcn',{@gaplotbestindiv, @gaplotbestf},...
+    'MaxGenerations',50, ...
+    'MaxStallGenerations',1, ...
+    'FunctionTolerance',10);
+[lMax,tMax] = ga(@(l) fun(l,[tau1;tau2]),2,[],[],[],[],lb,ub,[],optimOpts); 
+% sometimes error无法执行赋值，因为左侧的大小为 1×1，右侧的大小为 0×0。
+tMax = -tMax;
+
+%% 
+rmpath(genpath('funcs'));
+
+%% 
+function te = fun(l,tau)
 tspan = [0 500];
 q0 = [0;0;0;0];
 odeOpt = odeset('RelTol',1e-3,'AbsTol',1e-5,'InitialStep',0.001, ...
     'Events',@(t,y)isEnd(t,y,l(1),l(2)));
-[~,~,te,~,~] = ode45(@(t,y)newtonEuler(t,y,l(1),l(2),tau1,tau2), tspan, q0, odeOpt);
-te = -te;
+[~,~,te,~,~] = ode45(@(t,y)newtonEuler(t,y,l(1),l(2),tau(1),tau(2)), tspan, q0, odeOpt);
+te = -te; % ga: find the minimum value
 
 end
-
-%% 
